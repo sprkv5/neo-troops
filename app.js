8314d0a47d0adabfd1ba5b7a78386c767744ae35
Subhashish Pradhan <sprkv5@gmail.com>
2015-12-30 21:31:52 +0530

    Properly fetches inputs

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
-var name;
-
-
 $(document).ready(function(){
-  $("#setColors").click(function(e){
-    e.preventDefault();
+$("#setColors").click(storeInput);
+//$("#train").click();
+//$("#test").click();
+});
 
-    name = $("#name").val();
+var storeInput = function(){
+
+    var name = $("#name").val();
     var age = $("#age").val();
-    var gender = $("#gender input:radio:checked").val();
-    var testType = $("#testType input:radio:checked").val();
+    var gender = $("input[name=genderOptions]:checked").val();
+    var testType = $("input[name=typeOptions]:checked").val();
     var testDuration = $("#testDuration").val();
     var nColors = $("#number").val();
 
+    /*
+    console.log(name);
+    console.log(age);
+    console.log(gender);
+    console.log(testType);
+    console.log(testDuration);
+    console.log(nColors);
+    */
 
+    $('select[name="colorpicker-modal-picker"]').simplecolorpicker();
+    $('select[name="colorpicker-modal-picker"]').simplecolorpicker('selectColor', '#7bd148');
+    $('select[name="colorpicker-modal-picker"]').simplecolorpicker('destroy');    
+
+    
+};
+
+
+
+    //var clog = function() {
+    //    console.log(name); // does not log name ??
+    //}
+
+    //e.preventDefault();
     //var x = $("#keyword").val();
     //var y = $("#location").val();
     //$("#keyword").attr("value", x);
     //$("#location").attr("value", y);
-  });
-  $("#test").click(clog);
-});
 
-    var clog = function() {
-        console.log(name); // does not log name ??
-    }
+    //$("#test").click(clog);
